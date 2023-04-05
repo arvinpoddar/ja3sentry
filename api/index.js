3599d4cf07bbfb22c8cd0f00424e65610c3bd902
Arvin Poddar <arvin.poddar@gmail.com>
2023-04-05 01:51:31 -0400

    Add potential threat POST endpoint

--- api/index.js
+++ api/index.js
@@ -58,6 +58,54 @@ app.post(VERIFY_ENDPOINT, (request, response) => {
   }
 });
 
+/**
+ * Handles the addition of a JA3 Block to the Potential Threat table. Request
+ * body should be formatted as
+ *
+ * {
+ *  ja3: '...',
+ *  ja3_md5: '...',
+ *  ja3_sha1: '...',
+ *  user_agent: '...',
+ *  collected_at: '...'
+ * }
+ */
+const POTENTIAL_THREAT_ENDPOINT = "/api/potential-threat";
+app.post(POTENTIAL_THREAT_ENDPOINT, (request, response) => {
+  console.log(
+    `${POTENTIAL_THREAT_ENDPOINT} received request from ${request.ip}`
+  );
+  try {
+    const itemToValue = (item) => {
+      return item ? `'${item}'` : "null";
+    };
+
+    const ja3 = itemToValue(request.body.ja3);
+    const ja3md5 = itemToValue(request.body.ja3_md5);
+    const ja3sha1 = itemToValue(request.body.ja3_sha1);
+    const userAgent = itemToValue(request.body.user_agent);
+    const collectedAt = itemToValue(request.body.collected_at);
+
+    if (!(ja3 || ja3md5 || ja3sha1 || userAgent || collectedAt)) {
+      return response.send(0);
+    }
+
+    const queryString = `INSERT INTO potential(ja3, ja3_md5, ja3_sha1, user_agent, collected_at) VALUES (${ja3},${ja3md5},${ja3sha1},${userAgent},${collectedAt});`;
+
+    console.log(queryString);
+
+    db.query(queryString, (error, result) => {
+      if (error) {
+        return response.status(500).send(error);
+      }
+
+      return response.send(result.rows || []);
+    });
+  } catch (error) {
+    return response.status(500).send(error);
+  }
+});
+
 const API_PORT = process.env.APPLICATION_PORT || 4000;
 app.listen(API_PORT, () => {
   console.log(`API listening on port ${API_PORT}`);
