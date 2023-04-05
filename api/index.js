33e4438c6629ff6f4f128086fc4efb3334700ab9
Arvin Poddar <arvin.poddar@gmail.com>
2023-04-05 01:35:03 -0400

    Add verification endpoint

--- api/index.js
+++ api/index.js
@@ -18,15 +18,40 @@ app.get("/api/check", (req, res) => {
  * received blocks that are deemed threats based on database matches
  */
 const VERIFY_ENDPOINT = "/api/verify";
-app.get(VERIFY_ENDPOINT, (request, response) => {
+app.post(VERIFY_ENDPOINT, (request, response) => {
   console.log(`${VERIFY_ENDPOINT} received request from ${request.ip}`);
   try {
-    db.query("SELECT * FROM threats", (error, result) => {
+    const ja3Block = request.body.ja3Block;
+    if (!(ja3Block && ja3Block.length)) {
+      return response.send([]);
+    }
+
+    const arrayToQueryCollection = (arr) => {
+      arr.push("null");
+      return arr
+        .filter((i) => !!i)
+        .map((i) => `'${i}'`)
+        .join(",");
+    };
+
+    const md5s = ja3Block.map((block) => block.ja3_md5);
+    const sha1s = ja3Block.map((block) => block.ja3_sha1);
+    const userAgents = ja3Block.map((block) => block.user_agent);
+
+    const queryString = `SELECT * FROM threats WHERE ja3_md5 IN (${arrayToQueryCollection(
+      md5s
+    )}) OR ja3_sha1 IN (${arrayToQueryCollection(
+      sha1s
+    )}) OR user_agent IN (${arrayToQueryCollection(userAgents)});`;
+
+    console.log(queryString);
+
+    db.query(queryString, (error, result) => {
       if (error) {
         return response.status(500).send(error);
       }
 
-      response.send(result);
+      return response.send(result.rows || []);
     });
   } catch (error) {
     return response.status(500).send(error);
